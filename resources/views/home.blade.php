8d2590f1a7d108ba3bdefff3e859c4388311f2c6
dafsy31 <cowriter.id@gmail.com>
2025-12-04 16:46:56 +0700

    update title

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>STEM Literacy | OER Commons</title>
-    <link rel="icon" type="image/png" href="https://oercommons.org/static/images/favicon.ico">
-
+    <title>CENTRE STEAM</title>
+    {{-- <link rel="icon" type="image/png" href="https://oercommons.org/static/images/favicon.ico"> --}}
 
     <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
